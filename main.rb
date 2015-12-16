ee86ba43dd01f615819f39ae03e14a21a68a78ba
Nick Rank <ranknc@s.dcsdk12.org>
2015-12-16 09:47:22 -0700

    Did my best

--- main.rb
+++ main.rb
@@ -18,6 +18,8 @@ class GameWindow < Gosu::Window
 		@orb_anim = Gosu::Image::load_tiles("Images/orb.png", 10, 10)
 		@orbs = []
 		@font = Gosu::Font.new(20)
+		@start_time = 180
+		@end_time = 0
 		
 	end
 
@@ -36,6 +38,10 @@ class GameWindow < Gosu::Window
 		@orbs.each do |orb|
 			@orbs.delete(orb) if orb.collected? == 1
 		end
+		if @start_time < @end_time
+			close
+			print "Game Over! Time Expired!"
+		end
 	end
 
 	def draw
@@ -43,6 +49,7 @@ class GameWindow < Gosu::Window
 		@background_image.draw(0, 0, ZOrder::BACKGROUND)
 		@orbs.each { |orb| orb.draw }
 		@font.draw("Score: #{@player.score}", 10, 10, ZOrder::UI, 1.0, 1.0, 0xff_ffff00)
+		@font.draw("Time: #{(@start_time = @start_time - 0.020).to_i}", 10, 30, ZOrder::UI, 1.0, 1.0, 0xff_ffff00)
 	end
 
 	def button_down(id)
